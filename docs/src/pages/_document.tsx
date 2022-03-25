8e0ef86e26a078f8c17f32eca525c0026da0f8ec
RavenInd <sam@focusreactive.com>
2022-03-25 14:25:12 +0400

    added assets

--- docs/src/pages/_document.tsx
+++ docs/src/pages/_document.tsx
@@ -3,6 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
+      <Head>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
