f5c3fa7f647687b4d954e830c337a9c0cca1393d
Kragov <robrozdestvensky@gmail.com>
2022-03-29 17:11:23 +0300

    Add BuiltByBlock and stories for it

---
+++ docs/src/components/landingBlocks/BuiltByBlock.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import BuiltByBlock from "./BuiltByBlock";
+
+export default {
+  title: "Landing Blocks/Built by",
+};
+
+export const Default = () => <BuiltByBlock />;
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import LandingLayout from "../components/LandingLayout";
 import HeroBlock from "../components/landingBlocks/HeroBlock";
 import WhyHardhatBlock from "../components/landingBlocks/WhyHardhatBlock";
+import BuiltByBlock from "../components/landingBlocks/BuiltByBlock";
 // import ToolsBlock from '../components/landingBlocks/ToolsBlock';
 
 const Home: NextPage = () => {
@@ -9,6 +10,7 @@ const Home: NextPage = () => {
     <LandingLayout seo={{ title: "Hardhat" }}>
       <HeroBlock />
       <WhyHardhatBlock />
+      <BuiltByBlock />
 
       {/* Required confirmation from customers */}
       {/* <ToolsBlock /> */}
