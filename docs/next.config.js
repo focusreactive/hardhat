6a07f8e6d63028f88e5cf5ca76fa79e599451ff8
oleg proskurin <regx@usul.su>
2022-03-30 21:16:39 +0700

    Fix Storybook config

--- docs/next.config.js
+++ docs/next.config.js
@@ -12,25 +12,26 @@ const withMDX = require("@next/mdx")({
   },
 });
 
-const nextConfig = withMDX(
-  withLinaria({
-    reactStrictMode: true,
-    future: {
-      webpack5: true,
-    },
-    webpack(baseConfig) {
-      // eslint-disable-next-line no-param-reassign
-      baseConfig.resolve.alias.theme = path.resolve(__dirname, "./src/themes");
-      return baseConfig;
-    },
-    linaria: {
-      cacheDirectory:
-        process.env.NODE_ENV === "production"
-          ? ".next/cache/.linaria-cache"
-          : ".linaria-cache",
-    },
-    pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-  })
-);
+const linariaConfig = withLinaria({
+  reactStrictMode: true,
+  future: {
+    webpack5: true,
+  },
+  webpack(baseConfig) {
+    // eslint-disable-next-line no-param-reassign
+    baseConfig.resolve.alias.theme = path.resolve(__dirname, "./src/themes");
+    return baseConfig;
+  },
+  linaria: {
+    cacheDirectory:
+      process.env.NODE_ENV === "production"
+        ? ".next/cache/.linaria-cache"
+        : ".linaria-cache",
+  },
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+});
+
+const nextConfig = withMDX(linariaConfig);
+nextConfig.linariaConfig = linariaConfig;
 
 module.exports = nextConfig;
