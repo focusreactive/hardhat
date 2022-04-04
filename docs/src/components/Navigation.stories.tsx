f11b9bb6bda2d818e56c28e7b4c131fb200173fc
RavenInd <sam@focusreactive.com>
2022-04-04 19:21:02 +0400

    refactor: add documentation menu to storybook

---
+++ docs/src/components/Navigation.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { default as DocumentsNavigation } from "./Navigation";
+
+export default {
+  title: "Common/Documents Navigation",
+};
+
+export const Navigation = () => <DocumentsNavigation />;
