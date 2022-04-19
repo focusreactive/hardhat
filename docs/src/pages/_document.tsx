1ad6e7a8ce16d9d989f001d31fddf00ac19442ed
oleg proskurin <regx@usul.su>
2022-04-19 16:45:45 +0700

    Optimize fonts loading

--- docs/src/pages/_document.tsx
+++ docs/src/pages/_document.tsx
@@ -3,11 +3,33 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/Chivo-Bold.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="true"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Chivo-Regular.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="true"
+        />
+        <link
+          rel="preload"
+          href="/fonts/Chivo-Light.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="true"
+        />
+      </Head>
       <body>
         <script
           dangerouslySetInnerHTML={{
-            __html: ` 
+            __html: `
           const theme = localStorage.getItem('theme') || 'AUTO';
           document.body.className = theme;`,
           }}
