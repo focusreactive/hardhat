29923a2d0be1e606554c0a52fa9f957dd8dade2e
RavenInd <sam@focusreactive.com>
2022-04-28 21:03:50 +0400

    refactor: fix lint issues

--- docs/src/components/mdxComponents/MDImage.tsx
+++ docs/src/components/mdxComponents/MDImage.tsx
@@ -28,11 +28,11 @@ const MDImage = ({ src, alt }: Props) => {
         className="md-img"
         src={src}
         alt={alt}
-        width={"100%"}
-        height={"100%"}
-        layout="responsive"
         placeholder="blur"
         blurDataURL={src}
+        width="100%"
+        height="100%"
+        layout="responsive"
         objectFit="contain"
       />
     </ImageContainer>
