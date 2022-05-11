3b6f725d9dfaee8c6236de17f0ac85281248d6bf
RavenInd <sam@focusreactive.com>
2022-05-11 18:20:06 +0400

    refactor: fix lint issue

--- docs/src/components/landingBlocks/ToolsBlock.tsx
+++ docs/src/components/landingBlocks/ToolsBlock.tsx
@@ -135,8 +135,6 @@ const ToolsBlock = ({ content }: BlockProps) => {
 
   useEffect(() => {
     const queryTool = toolRegExp.exec(router.asPath);
-    console.log(queryTool);
-
     if (!queryTool) return;
     const tool = queryTool[0].replace("tool=", "") as Tools;
     setSelectedTool(tool);
