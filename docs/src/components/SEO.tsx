bc1278159860752305429e03303221576a7e0d86
oleg proskurin <regx@usul.su>
2022-05-11 14:37:17 +0700

    Update metatags

--- docs/src/components/SEO.tsx
+++ docs/src/components/SEO.tsx
@@ -11,10 +11,10 @@ interface Props {
 const SEO = ({ seo }: Props) => {
   return (
     <Head>
-      <title>{`${seo.title} | ${seo.description}`}</title>
+      <title>{`${seo.title} | Ethereum development environment for professionals by Nomic Foundation`}</title>
       <meta name="description" content={seo.description} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="shortcut icon" href="/static/favicon.ico" />
+      <link rel="shortcut icon" href="/favicon.ico" />
     </Head>
   );
 };
